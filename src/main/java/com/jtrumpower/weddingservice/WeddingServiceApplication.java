4523bd3b5dc28fa05e61d945969751f70f4893cc
jtrumpower <trumpower102@gmail.com>
2017-07-15 13:32:19 -0400

    project commit

---
+++ src/main/java/com/jtrumpower/weddingservice/WeddingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.jtrumpower.weddingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingServiceApplication.class, args);
+	}
+}
